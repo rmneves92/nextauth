257526e0ca69d5f47cbf778783575d09fa135694
Rafael Neves <rafa.mneves@hotmail.com>
2021-07-13 08:28:11 -0300

    add user logout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a2cae17174634412956f";
+/******/ 	var hotCurrentHash = "c9fde58adbf9005cc079";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
-import { useSession } from 'next-auth/client';
+import { useSession, signOut } from 'next-auth/client';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
   const [session, loading] = useSession();
 
-  console.log(loading);
-  console.log(session);
+  function logoutHandler() {
+    signOut();
+  }
 
   return (
     <header className={classes.header}>
@@ -31,7 +32,7 @@ function MainNavigation() {
 
           {session && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
