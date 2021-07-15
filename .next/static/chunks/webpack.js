6656a2d980f57191b31f0a37c7ebdf9676e77309
Rafael Neves <rafa.mneves@hotmail.com>
2021-07-15 08:53:53 -0300

    Revert "add change password logic"
    This reverts commit 15c0dc09fc0ee823d9716f5de4d358d49fbc2877.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48cbfdf074168c4edd46";
+/******/ 	var hotCurrentHash = "cc55c493ed3fe88985ad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
