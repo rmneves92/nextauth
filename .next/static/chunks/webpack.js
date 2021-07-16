dd12dd6c5525f6d9e6536f490bc816378cf9d034
Rafael Neves <rafa.mneves@hotmail.com>
2021-07-15 23:48:45 -0300

    request change password from the frontend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cc55c493ed3fe88985ad";
+/******/ 	var hotCurrentHash = "2a45b4e0978ced99e4fe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -18,10 +18,25 @@ function UserProfile() {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
 
+  async function changePasswordHandler(passwordData) {
+    console.log('passwordData', passwordData);
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      bofy: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
